36bcb2b276c977ea90fc903a37dbb858bfdbb5d0
Vamsi Krishna Nalluri <vamckrishna.nalluri@gmailc.com>
2024-02-06 22:08:11 +0530

    Section 15: kubernetes files .

--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-mvn -pl ./accounts compile jib:dockerBuild
+#mvn -pl ./accounts compile jib:dockerBuild
+#
+#mvn -pl ./loans compile jib:dockerBuild
+#
+#mvn -pl ./cards compile jib:dockerBuild
+#
+#mvn -pl ./configserver compile jib:dockerBuild
+#
+#mvn -pl ./eurekaserver compile jib:dockerBuild
+#
+#mvn -pl ./gatewayserver compile jib:dockerBuild
 
-mvn -pl ./loans compile jib:dockerBuild
+VERSION=s14
 
-mvn -pl ./cards compile jib:dockerBuild
+mvn clean package -DskipTests
 
-mvn -pl ./configserver compile jib:dockerBuild
-
-mvn -pl ./eurekaserver compile jib:dockerBuild
-
-mvn -pl ./gatewayserver compile jib:dockerBuild
+docker build -t vamckrishna/configserver:"$VERSION" ./configserver
+docker build -t vamckrishna/accounts:"$VERSION" ./accounts
+docker build -t vamckrishna/loans:"$VERSION" ./loans
+docker build -t vamckrishna/cards:"$VERSION" ./cards
+docker build -t vamckrishna/eurekaserver:"$VERSION" ./eurekaserver
+docker build -t vamckrishna/gatewayserver:"$VERSION" ./gatewayserver
+docker build -t vamckrishna/message:"$VERSION" ./message
